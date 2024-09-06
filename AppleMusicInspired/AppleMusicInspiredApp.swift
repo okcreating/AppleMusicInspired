8941409adf6eaadbb23963acf94c7d271f60bd9a
okcreating <oksibearus@gmail.com>
2024-09-06 13:09:14 +0100

    Initial Commit

---
+++ AppleMusicInspired/AppleMusicInspiredApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicInspiredApp.swift
+//  AppleMusicInspired
+//
+//  Created by Oksana Kazarinova on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicInspiredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
