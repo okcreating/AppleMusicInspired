250fce620263b83997ffe879430f7fe0ff757026
okcreating <oksibearus@gmail.com>
2024-11-11 19:29:12 +0000

    album photo and blurred background

--- AppleMusicInspired/AudioPlayer/FullScreenPlayerView.swift
+++ AppleMusicInspired/AudioPlayer/FullScreenPlayerView.swift
@@ -22,71 +22,104 @@ struct FullScreenPlayerView: View {
 //    let color: AverageColor?
 
     var body: some View {
-        ZStack {
+        GeometryReader {
+            let size = $0.size
+            let safeArea = $0.safeAreaInsets
 
-            VStack {
-                Spacer()
-                Image(uiImage: song.image)
-                    .resizable()
-                    .frame(width: 260, height: 260, alignment: .center)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            ZStack {
+                Rectangle()
+                    .fill(.ultraThickMaterial)
+                    .overlay(content: {
+                        Rectangle()
+                        song.image
+                            .blur(radius: 55)
+                          //  .opacity(animationContent ? 1 : 0)
+                    })
+                 //   .matchedGeometryEffect(id:"Background", in: animation)
 
-                Spacer()
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(song.name)
-                            .fontWeight(.bold)
-                        Text(song.artist)
+                VStack(spacing: 15) {
+                    GeometryReader {
+                        let size = $0.size
+                        song.image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: size.width, height: size.height)
+                            .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
                     }
-                    .font(.title)
-                    .padding()
-                    Spacer()
-                }
-                ProgressView(value: 0.3)
-                    .progressViewStyle(.linear)
-                    .padding(.leading)
-                    .padding(.trailing)
-                HStack {
-                    Text("0:00")
-                    Spacer()
-                    Text("3:24")
-                }
-                .padding(.leading)
-                .padding(.trailing)
-                .font(.system(size: 14))
-                Spacer()
-                HStack {
-                    Image(systemName: "backward.fill")
-                    Spacer()
-                    Image(systemName: "play.fill")
-                    Spacer()
-                    Image(systemName: "forward.fill")
-                }
-                .font(.system(size: 50))
-                .padding(30)
+                    .frame(height: size.width - 50)
+                    .padding(.vertical, size.height < 700 ? 10 : 30)
+                    }
+                    .padding(.top, safeArea.top + (safeArea.bottom == 0 ? 10 : 0))
+                    .padding(.bottom, safeArea.bottom + (safeArea.bottom == 0 ? 10 : safeArea.bottom))
+                    .padding(.horizontal, 25)
+                    .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .top)
+                    .clipped()
 
-                Spacer()
-                HStack {
-                    Image(systemName: "speaker.fill")
-                    ProgressView(value: 0.3)
-                    Image(systemName: "speaker.wave.3.fill")
-                }
-                .padding(.leading)
-                .padding(.trailing)
-
-
-                HStack {
-                    Image(systemName: "quote.bubble")
-                    Image(systemName: "airplay.audio.circle.fill")
-                    Image(systemName: "list.bullet.indent")
-                }
-                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-                Spacer()
-                   //.backgroundStyle(AverageColor.generate(from: song.image.render()!).averageColor)
             }
         }
 
+//            VStack {
+//                Spacer()
+//                song.image
+//                    .resizable()
+//                    .frame(width: 260, height: 260, alignment: .center)
+//                    .clipShape(RoundedRectangle(cornerRadius: 10))
+//
+//                Spacer()
+//                HStack {
+//                    VStack(alignment: .leading) {
+//                        Text(song.name)
+//                            .fontWeight(.bold)
+//                        Text(song.artist)
+//                    }
+//                    .font(.title)
+//                    .padding()
+//                    Spacer()
+//                }
+//                ProgressView(value: 0.3)
+//                    .progressViewStyle(.linear)
+//                    .padding(.leading)
+//                    .padding(.trailing)
+//                HStack {
+//                    Text("0:00")
+//                    Spacer()
+//                    Text("3:24")
+//                }
+//                .padding(.leading)
+//                .padding(.trailing)
+//                .font(.system(size: 14))
+//                Spacer()
+//                HStack {
+//                    Image(systemName: "backward.fill")
+//                    Spacer()
+//                    Image(systemName: "play.fill")
+//                    Spacer()
+//                    Image(systemName: "forward.fill")
+//                }
+//                .font(.system(size: 50))
+//                .padding(30)
+//
+//                Spacer()
+//                HStack {
+//                    Image(systemName: "speaker.fill")
+//                    ProgressView(value: 0.3)
+//                    Image(systemName: "speaker.wave.3.fill")
+//                }
+//                .padding(.leading)
+//                .padding(.trailing)
+//
+//
+//                HStack {
+//                    Image(systemName: "quote.bubble")
+//                    Image(systemName: "airplay.audio.circle.fill")
+//                    Image(systemName: "list.bullet.indent")
+//                }
+//                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+//                Spacer()
+//                   //.backgroundStyle(AverageColor.generate(from: song.image.render()!).averageColor)
+//            }
     }
+
     private func setupAudio() {
         guard let url = Bundle.main.url(forResource: song.name, withExtension: "mp3") else { return }
         do {
