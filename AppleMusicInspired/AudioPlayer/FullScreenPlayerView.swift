9abea828cfd65cc665a4549595cec0801e240db4
okcreating <oksibearus@gmail.com>
2024-11-12 00:03:35 +0000

    full screen player view finished

--- AppleMusicInspired/AudioPlayer/FullScreenPlayerView.swift
+++ AppleMusicInspired/AudioPlayer/FullScreenPlayerView.swift
@@ -19,7 +19,7 @@ struct FullScreenPlayerView: View {
     @Binding var expandSheet: Bool
     var animation: Namespace.ID
     @State private var animationContent = false
-//    let color: AverageColor?
+    //    let color: AverageColor?
 
     var body: some View {
         GeometryReader {
@@ -33,9 +33,9 @@ struct FullScreenPlayerView: View {
                         Rectangle()
                         song.image
                             .blur(radius: 55)
-                          //  .opacity(animationContent ? 1 : 0)
+                        //  .opacity(animationContent ? 1 : 0)
                     })
-                 //   .matchedGeometryEffect(id:"Background", in: animation)
+                //   .matchedGeometryEffect(id:"Background", in: animation)
 
                 VStack(spacing: 15) {
                     GeometryReader {
@@ -48,76 +48,85 @@ struct FullScreenPlayerView: View {
                     }
                     .frame(height: size.width - 50)
                     .padding(.vertical, size.height < 700 ? 10 : 30)
-                    }
-                    .padding(.top, safeArea.top + (safeArea.bottom == 0 ? 10 : 0))
-                    .padding(.bottom, safeArea.bottom + (safeArea.bottom == 0 ? 10 : safeArea.bottom))
-                    .padding(.horizontal, 25)
-                    .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .top)
-                    .clipped()
 
+                    playerView(size)
+                    //.offset(y: animationContent ? 0)
+                }
+                .padding(.top, safeArea.top + (safeArea.bottom == 0 ? 10 : 0))
+                .padding(.bottom, safeArea.bottom + (safeArea.bottom == 0 ? 10 : safeArea.bottom))
+                .padding(.horizontal, 25)
+                .frame(maxWidth: .infinity,maxHeight: .infinity, alignment: .top)
+                .clipped()
             }
+            .ignoresSafeArea(.container, edges: .all)
+        }
+        .onAppear(perform: {
+            setupAudio()
+        })
+        .onReceive(Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()) { _ in
+            updateProgress()
         }
 
-//            VStack {
-//                Spacer()
-//                song.image
-//                    .resizable()
-//                    .frame(width: 260, height: 260, alignment: .center)
-//                    .clipShape(RoundedRectangle(cornerRadius: 10))
-//
-//                Spacer()
-//                HStack {
-//                    VStack(alignment: .leading) {
-//                        Text(song.name)
-//                            .fontWeight(.bold)
-//                        Text(song.artist)
-//                    }
-//                    .font(.title)
-//                    .padding()
-//                    Spacer()
-//                }
-//                ProgressView(value: 0.3)
-//                    .progressViewStyle(.linear)
-//                    .padding(.leading)
-//                    .padding(.trailing)
-//                HStack {
-//                    Text("0:00")
-//                    Spacer()
-//                    Text("3:24")
-//                }
-//                .padding(.leading)
-//                .padding(.trailing)
-//                .font(.system(size: 14))
-//                Spacer()
-//                HStack {
-//                    Image(systemName: "backward.fill")
-//                    Spacer()
-//                    Image(systemName: "play.fill")
-//                    Spacer()
-//                    Image(systemName: "forward.fill")
-//                }
-//                .font(.system(size: 50))
-//                .padding(30)
-//
-//                Spacer()
-//                HStack {
-//                    Image(systemName: "speaker.fill")
-//                    ProgressView(value: 0.3)
-//                    Image(systemName: "speaker.wave.3.fill")
-//                }
-//                .padding(.leading)
-//                .padding(.trailing)
-//
-//
-//                HStack {
-//                    Image(systemName: "quote.bubble")
-//                    Image(systemName: "airplay.audio.circle.fill")
-//                    Image(systemName: "list.bullet.indent")
-//                }
-//                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-//                Spacer()
-//                   //.backgroundStyle(AverageColor.generate(from: song.image.render()!).averageColor)
-//            }
+        //            VStack {
+        //                Spacer()
+        //                song.image
+        //                    .resizable()
+        //                    .frame(width: 260, height: 260, alignment: .center)
+        //                    .clipShape(RoundedRectangle(cornerRadius: 10))
+        //
+        //                Spacer()
+        //                HStack {
+        //                    VStack(alignment: .leading) {
+        //                        Text(song.name)
+        //                            .fontWeight(.bold)
+        //                        Text(song.artist)
+        //                    }
+        //                    .font(.title)
+        //                    .padding()
+        //                    Spacer()
+        //                }
+        //                ProgressView(value: 0.3)
+        //                    .progressViewStyle(.linear)
+        //                    .padding(.leading)
+        //                    .padding(.trailing)
+        //                HStack {
+        //                    Text("0:00")
+        //                    Spacer()
+        //                    Text("3:24")
+        //                }
+        //                .padding(.leading)
+        //                .padding(.trailing)
+        //                .font(.system(size: 14))
+        //                Spacer()
+        //                HStack {
+        //                    Image(systemName: "backward.fill")
+        //                    Spacer()
+        //                    Image(systemName: "play.fill")
+        //                    Spacer()
+        //                    Image(systemName: "forward.fill")
+        //                }
+        //                .font(.system(size: 50))
+        //                .padding(30)
+        //
+        //                Spacer()
+        //                HStack {
+        //                    Image(systemName: "speaker.fill")
+        //                    ProgressView(value: 0.3)
+        //                    Image(systemName: "speaker.wave.3.fill")
+        //                }
+        //                .padding(.leading)
+        //                .padding(.trailing)
+        //
+        //
+        //                HStack {
+        //                    Image(systemName: "quote.bubble")
+        //                    Image(systemName: "airplay.audio.circle.fill")
+        //                    Image(systemName: "list.bullet.indent")
+        //                }
+        //                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+        //                Spacer()
+        //                   //.backgroundStyle(AverageColor.generate(from: song.image.render()!).averageColor)
+        //            }
     }
 
     private func setupAudio() {
@@ -153,7 +162,118 @@ struct FullScreenPlayerView: View {
     private func timeStrirng(time: TimeInterval) -> String {
         let minutes = Int(time) / 60
         let seconds = Int(time) % 60
-return String(format: "%02d:%02d", minutes, seconds)
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+
+    @ViewBuilder
+    func playerView(_ mainSize: CGSize) -> some View {
+
+        GeometryReader {
+            let size = $0.size
+            let spacing = size.height * 0.04
+
+            VStack(spacing: spacing) {
+                VStack(spacing: spacing) {
+                    HStack(alignment: .center, spacing: 15) {
+                        VStack(alignment: .leading) {
+                            Text(song.name)
+                                .fontWeight(.semibold)
+                                .font(.title3)
+                            Text(song.artist)
+                                .foregroundStyle(.gray)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+
+                        Button {
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(.white)
+                                .padding(12)
+                                .background {
+                                    Circle()
+                                        .fill(.ultraThinMaterial)
+                                        .environment(\.colorScheme, .light)
+                                }
+                        }
+                    }
+                    Slider(value: Binding(get: {
+                        songCurrentTime
+                    }, set: { newValue in
+                        seekAudio(to: newValue)
+                    }), in: 0...songTotalTime)
+                    .foregroundColor(.white)
+
+                    HStack {
+                        Text(timeStrirng(time: songCurrentTime))
+                        Spacer()
+                        Text(timeStrirng(time: songTotalTime))
+                    }
+                }
+                .frame(height: size.height / 2.5, alignment: .top)
+
+                HStack(spacing: size.width * 0.2) {
+                    Button {
+                    } label : {
+                        Image(systemName: "backward.fill")
+                            .font(size.height < 300 ? .title3 : .title)
+                    }
+
+                    Button {
+                    } label : {
+                        Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+                            .font(size.height < 300 ? .largeTitle : .system(size: 50))
+                            .onTapGesture {
+                                isPlaying ? stopPlaying() : playAudio()
+                            }
+                    }
+
+                    Button {
+                    } label : {
+                        Image(systemName: "forward.fill")
+                            .font(size.height < 300 ? .title3 : .title)
+                    }
+                }
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+
+                VStack(spacing: spacing) {
+                    HStack(spacing: 15){
+                        Image(systemName: "speaker.fill")
+                        Capsule()
+                            .fill(.ultraThinMaterial)
+                            .environment(\.colorScheme, .light)
+                            .frame(height: 5)
+                        Image(systemName: "speaker.wave.3.fill")
+                    }
+                    .foregroundColor(.white)
+
+                    HStack(alignment: .top, spacing: size.width * 0.2) {
+
+                        Button {
+                        } label: {
+                            Image(systemName: "quote.bubble")
+                                .font(.title2)
+                        }
+
+                        Button {
+                        } label: {
+                            Image(systemName: "dot.radiowaves.left.and.right")
+                                .font(.title2)
+                        }
+
+                        Button {
+                        } label: {
+                            Image(systemName: "list.bullet")
+                                .font(.title2)
+                        }
+                    }
+                    .foregroundColor(.white)
+                    //.blendMode(.overlay)
+                    .padding(.top, spacing)
+                }
+               .frame(height: size.height / 2.5, alignment: .bottom)
+            }
+        }
     }
 }
 
